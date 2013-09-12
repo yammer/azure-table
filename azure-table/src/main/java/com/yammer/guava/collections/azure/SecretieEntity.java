ecb363863ec99c916adfea36747f4160627afc52
Michał Rutkowski <mrutkowski@yammer-inc.com>
2013-09-12 13:34:53 +0100

    seperated the azure table impl for secrets out of lib, effectivelyt removing the libs dep on a particular implementation, and making a first step to making the table implementation generic

---
+++ azure-table/src/main/java/com/yammer/guava/collections/azure/SecretieEntity.java
@@ -0,0 +1,38 @@
+package com.yammer.guava.collections.azure;
+
+import com.microsoft.windowsazure.services.table.client.Ignore;
+import com.microsoft.windowsazure.services.table.client.TableServiceEntity;
+import com.yammer.secretie.api.model.Key;
+import com.yammer.secretie.api.model.Secret;
+
+public class SecretieEntity extends TableServiceEntity {
+    /*package*/ static final String PARTITION_KEY = "secretie_single_partition"; // TODO move
+    private byte[] secret;
+
+    public SecretieEntity() { // needed by azure java api
+    }
+
+    public SecretieEntity(String rowKey, Key columnKey, Secret secret) {
+        this.partitionKey = rowKey;
+        this.rowKey = AzureKeyCodec.encode(columnKey);
+        this.secret = secret.asBytes();
+    }
+
+    @Ignore
+    public Key getKey() {
+        return AzureKeyCodec.decode(this.rowKey);
+    }
+
+    @Ignore
+    public Secret getSecret() {
+        return new Secret(this.secret);
+    }
+
+    public byte[] getSecretValue() {
+        return secret;
+    }
+
+    public void setSecretValue(byte[] secret) {
+        this.secret = secret;
+    }
+}
